33803ecd912e27424f352aa42d5664a488b4d41b
damola <damolaadegbite0@gmail.com>
2025-08-09 12:04:58 +0100

    feat: wire up AppModule

---
+++ services/payment-service/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PaymentModule } from './payment/payment.module';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+    PaymentModule,
+    RabbitMQModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
